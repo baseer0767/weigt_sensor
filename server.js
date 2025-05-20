0149b3b7c1a95f6921247e7929dff950d7a37d31
baseer0767 <sheikhabdulbaseer786@gmail.com>
2025-05-20 16:33:21 +0500

    Update project files

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 const uri = "mongodb://localhost:27017";
 const client = new MongoClient(uri);
 
